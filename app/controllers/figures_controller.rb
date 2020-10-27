82d29992161e068219d138e8ca792ca24fdf5ec8
fentyhall <fentymhall@gmail.com>
2020-10-26 21:20:55 -0700

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,3 +1,28 @@
 class FiguresController < ApplicationController
-  # add controller methods
+  get '/figures/new' do
+    erb :'figures/new'
+  end 
+
+  post '/figures' do 
+    # binding.pry 
+    @figure = Figure.create(params[:figure])
+    if !params[:title][:name].empty?
+      @title = Title.create(params[:title])
+      @figure.title_ids << params[:title][:id]
+    end 
+
+    if !params[:landmark][:name].empty? && !params[:landmark][:year_completed].empty?
+      @landmark = Landmark.create(params[:landmark])
+      @figure.landmark_ids << params[:landmarks]
+    end 
+
+    redirect to "/figures/#{@figure.id}"
+  end 
 end
+# if !params[:figure].keys.include?("title_ids")
+#   params[:figure]["title_ids"] = [] 
+# end 
+
+# if !params[:figure].keys.include?("landmark_ids")
+#   params[:figure]["landmark_ids"] = [] 
+# end 
